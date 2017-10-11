c6ae26f75b62cffe436671c61f663140709315a1
linghuam <1551827898@qq.com>
2017-10-11 18:15:07 +0800

    shiplayer

---
+++ linghuam/shipLayer.js
@@ -0,0 +1,130 @@
+L.ShipLayer = L.Path.extend({
+
+    options: {
+        fill: true,
+        rotate: 0,
+        width: 12,
+        height: 24
+    },
+
+    initialize: function (latlng, options) {
+        L.Util.setOptions(this, options);
+        this._latlng = this._toLatLng(latlng);
+        this._rotate = this.options.rotate;
+        this._width = this.options.width;
+        this._height = this.options.height;
+    },
+
+    setLatLng: function (latlng) {
+        this._latlng = this._toLatLng(latlng);
+        this.redraw();
+        return this.fire('move', { latlng: this._latlng });
+    },
+
+    getLatLng: function () {
+        return this._latlng;
+    },
+
+    setRotate: function (rotate) {
+        this.options.rotate = this._rotate = rotate;
+        return this.redraw();
+    },
+
+    getRotate: function () {
+        return this._rotate;
+    },
+
+    setStyle: function (options) {
+        var rotate = options && options.rotate || this._rotate;
+        Path.prototype.setStyle.call(this, options);
+        this.setRotate(rotate);
+        return this;
+    },
+
+    _project: function () {
+        this._point = this._map.latLngToLayerPoint(this._latlng);
+        this._updateBounds();
+    },
+
+    _updateBounds: function () {
+        var r = this._width,
+            r2 = this._height || r,
+            w = this._clickTolerance(),
+            p = [r + w, r2 + w];
+        this._pxBounds = new Bounds(this._point.subtract(p), this._point.add(p));
+    },
+
+    _update: function () {
+        if(this._map) {
+            this._updatePath();
+        }
+    },
+
+    _updatePath: function () {
+        this._renderer._updateShip(this);
+    },
+
+    _empty: function () {
+        return this._width && this._height && this._rotate && !this._renderer._bounds.intersects(this._pxBounds);
+    },
+
+    // Needed by the `Canvas` renderer for interactivity
+    _containsPoint: function (p) {
+        return p.distanceTo(this._point) <= this._width + this._clickTolerance();
+    }
+
+    _toLatLng: function (a, b, c) {
+        if(a instanceof LatLng) {
+            return a;
+        }
+        if(L.Util.isArray(a) && typeof a[0] !== 'object') {
+            if(a.length === 3) {
+                return new LatLng(a[0], a[1], a[2]);
+            }
+            if(a.length === 2) {
+                return new LatLng(a[0], a[1]);
+            }
+            return null;
+        }
+        if(a === undefined || a === null) {
+            return a;
+        }
+        if(typeof a === 'object' && 'lat' in a) {
+            return new LatLng(a.lat, 'lng' in a ? a.lng : a.lon, a.alt);
+        }
+        if(b === undefined) {
+            return null;
+        }
+        return new LatLng(a, b, c);
+    }
+});
+
+L.ShipCanvas = L.Canvas.extend({
+    _updateShip: function (layer) {
+        if(!this._drawing || layer._empty()) { return; }
+
+        var p = layer._point,
+            ctx = this._ctx,
+            rotate = layer._rotate,
+            w = layer._width,
+            h = layer._height,
+            dh = h / 3;
+
+        this._drawnLayers[layer._leaflet_id] = layer;
+
+        // 绘制部分 -start
+        ctx.save();
+        ctx.rotate((Math.PI / 180) * rotate);
+        ctx.beginPath();
+        ctx.moveTo(0, 0 - h / 2);
+        ctx.lineTo(0 - w / 2, 0 - h / 2 + dh);
+        ctx.lineTo(0 - w / 2, 0 + h / 2);
+        ctx.lineTo(0 + w / 2, 0 + h / 2);
+        ctx.lineTo(0 + w / 2, 0 - h / 2 + dh);
+        ctx.closePath();
+
+        this._fillStroke(ctx, layer);
+        ctx.restore();
+        // 绘制部分  -end 
+    }
+});
