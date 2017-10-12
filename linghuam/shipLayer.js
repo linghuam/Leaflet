f07cd4c166a6f16aa380e14f9222f04c70d5274d
linghuam <1551827898@qq.com>
2017-10-12 10:48:55 +0800

    shiplayer

--- linghuam/shipLayer.js
+++ linghuam/shipLayer.js
@@ -51,7 +51,7 @@ L.ShipLayer = L.Path.extend({
             r2 = this._height || r,
             w = this._clickTolerance(),
             p = [r + w, r2 + w];
-        this._pxBounds = new Bounds(this._point.subtract(p), this._point.add(p));
+        this._pxBounds = new L.Bounds(this._point.subtract(p), this._point.add(p));
     },
 
     _update: function () {
@@ -71,18 +71,18 @@ L.ShipLayer = L.Path.extend({
     // Needed by the `Canvas` renderer for interactivity
     _containsPoint: function (p) {
         return p.distanceTo(this._point) <= this._width + this._clickTolerance();
-    }
+    },
 
     _toLatLng: function (a, b, c) {
-        if(a instanceof LatLng) {
+        if(a instanceof L.LatLng) {
             return a;
         }
         if(L.Util.isArray(a) && typeof a[0] !== 'object') {
             if(a.length === 3) {
-                return new LatLng(a[0], a[1], a[2]);
+                return new L.LatLng(a[0], a[1], a[2]);
             }
             if(a.length === 2) {
-                return new LatLng(a[0], a[1]);
+                return new L.LatLng(a[0], a[1]);
             }
             return null;
         }
@@ -90,12 +90,12 @@ L.ShipLayer = L.Path.extend({
             return a;
         }
         if(typeof a === 'object' && 'lat' in a) {
-            return new LatLng(a.lat, 'lng' in a ? a.lng : a.lon, a.alt);
+            return new L.LatLng(a.lat, 'lng' in a ? a.lng : a.lon, a.alt);
         }
         if(b === undefined) {
             return null;
         }
-        return new LatLng(a, b, c);
+        return new L.LatLng(a, b, c);
     }
 });
 
@@ -114,6 +114,7 @@ L.ShipCanvas = L.Canvas.extend({
 
         // 绘制部分 -start
         ctx.save();
+        ctx.translate(p.x, p.y);
         ctx.rotate((Math.PI / 180) * rotate);
         ctx.beginPath();
         ctx.moveTo(0, 0 - h / 2);
@@ -126,5 +127,27 @@ L.ShipCanvas = L.Canvas.extend({
         this._fillStroke(ctx, layer);
         ctx.restore();
         // 绘制部分  -end 
+    },
+
+    _fillStroke: function (ctx, layer) {
+        var options = layer.options;
+
+        if(options.fill) {
+            ctx.globalAlpha = options.fillOpacity;
+            ctx.fillStyle = options.fillColor || options.color;
+            ctx.fill(options.fillRule || 'evenodd');
+        }
+
+        if(options.stroke && options.weight !== 0) {
+            if(ctx.setLineDash) {
+                ctx.setLineDash(layer.options && layer.options._dashArray || []);
+            }
+            ctx.globalAlpha = options.opacity;
+            ctx.lineWidth = options.weight;
+            ctx.strokeStyle = options.color;
+            ctx.lineCap = options.lineCap;
+            ctx.lineJoin = options.lineJoin;
+            ctx.stroke();
+        }
     }
 });
